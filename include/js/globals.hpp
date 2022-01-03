68d9b42fda2f6407786de4f639f0f64b322b4c5c
2pichar <77556662+2pichar@users.noreply.github.com>
2022-01-03 11:56:01 -0800

    added console class

--- include/js/globals.hpp
+++ include/js/globals.hpp
@@ -1,5 +1,11 @@
 #include "./number.hpp"
-#include <stdio.h>
+#include "./string.hpp"
+#include "./array.hpp"
+#include "./object.hpp"
+
+#include <iostream>
+
+
 namespace js{
     /*Console.log:
     template <typename ...Args>
@@ -9,6 +15,39 @@ namespace js{
         printf(arr.join().std() + '\n');
     }
     */
+    
+    class console {
+        private:
+            console();
+        public:
+            static void log(Object... args){
+                Array<Object> arr = {};
+                (arr.push(args), ...);
+                std::cout << (arr.join().std() + '\n');
+            }
+            static void error(Object... args){
+                Array<Object> arr = {};
+                (arr.push(args), ...);
+                std::cerr << (arr.join().std() + '\n');
+            }
+            static void warn(Object... args){
+                error(args...);
+            }
+            static void info(Object... args){
+                log(args...);
+            }
+            static void debug(Object... args){
+                log(args...);
+            }
+            static void trace(Object... args){
+                log(args...);
+            }
+            static void assert(bool condition, Object... args){
+                if(!condition){
+                    error(args...);
+                }
+            }
+    }
 
     class null {
 
