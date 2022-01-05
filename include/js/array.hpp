4249bb8abb66e37227fed122a3197907dee8ae81
2pichar <77556662+2pichar@users.noreply.github.com>
2022-01-05 15:05:09 -0800

    implemented array.prototype.filter and array.prototype.at

--- include/js/array.hpp
+++ include/js/array.hpp
@@ -2,15 +2,25 @@
 #include "./object.hpp"
 #include "./string.hpp"
 #include "./number.hpp"
+#include "./boolean.hpp"
+#include "./globals.hpp"
 
 namespace js {
+    typedef class Boolean Callback(class Object, class Number, class Array);
     class Array: public Object {
         private:
             std::vector<Object> value;
         public:
             Array();
-            static from();
+            static Array from(Object);
+            static Boolean isArray(Object);
             
             String join(String = ",");
+            Array filter(Callback);
+            Object at(Number);
+
+
+            static constexpr class String __type__ = "object";
+            friend String typeof(Object);
     }
 }
--- include/js/c++/array.cpp
+++ include/js/c++/array.cpp
@@ -3,15 +3,43 @@
 #include "../array.hpp"
 #include "../number.hpp"
 #include "../string.hpp"
+#include "../boolean.hpp"
+#include "../globals.hpp"
 
 namespace js {
-        Array::Array(){}
-        String Array::join(String sep = ","){
-            String result = "";
-            for(Number i = 0; i < this->length(); i++){
-                result += this[i];
-                if( i != this->length() - 1) result += sep;
-            }
-            return result;
+    typedef class Boolean Callback(class Object, class Number, class Array);
+    Array::Array(){}
+    String Array::join(String sep = ","){
+        class String result = "";
+        class Number i = 0;
+        for(i = 0; i < this->length(); i++){
+            result += this[i];
+            if( i != this->length() - 1) result += sep;
         }
+        return result;
+    }
+
+    Array Array::filter(Callback callback){
+        class Number i;
+        class Object el;
+        class Array arr = {};
+        for(i = 0; i < this->length(); i++){
+            el = this[i];
+            if(callback(el, i, this->value)){
+               arr.push(el); 
+            }
+        }
+        return arr;
+    }
+
+    Object Array::at(Number i){
+        // Allow for negative indices
+        if(i < 0) i += this->length();
+        
+        // OOB (Out-of-Bounds) access returns undefined
+        if(n < 0 || n > this->length()) return undefined;
+
+        // Normal access
+        return this[i];
+    }
 }
