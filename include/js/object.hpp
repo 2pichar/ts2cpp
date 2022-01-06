45bf8522167cf38e2539f441b02164328bddbb78
2pichar <77556662+2pichar@users.noreply.github.com>
2022-01-06 13:32:56 -0800

    updated types

--- include/js/object.hpp
+++ include/js/object.hpp
@@ -1,11 +1,15 @@
 #include <map> // Include header for maps/dictionaries
+#include <iostream>
 
 namespace js {
+    template <class K, class V> class Object;
+
     template <class K, class V>
-    class Object {
+    class Object<K, V> {
         private:
             std::map<K, V> dict;
         public:
             Object();
+            friend std::ostream& operator<<(std::ostream&, Object&);
     }
 }
