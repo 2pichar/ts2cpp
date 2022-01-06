7b310a00d334dcfc84f45fadc34e3a3293d55a6f
2pichar <77556662+2pichar@users.noreply.github.com>
2022-01-06 13:32:42 -0800

    updated types

--- include/js/array.hpp
+++ include/js/array.hpp
@@ -1,23 +1,30 @@
 #include <vector>
 #include "./object.hpp"
+#include "./function.hpp"
 #include "./string.hpp"
 #include "./number.hpp"
 #include "./boolean.hpp"
 #include "./globals.hpp"
 
 namespace js {
-    typedef class Boolean Callback(class Object, class Number, class Array);
-    class Array: public Object {
+    typedef class Boolean filter_f(class Object, class Number, class Array);
+    typedef class Object reduce_f(class Object, class Object, class Number, class Array);
+    template <typename V> class Array;
+
+    template <typename V>
+    class Array<V>: public Object {
+        typedef V value_t;
         private:
-            std::vector<Object> value;
+            class std::vector<V> value;
         public:
             Array();
             static Array from(Object);
             static Boolean isArray(Object);
             
             String join(String = ",");
-            Array filter(Callback);
-            Object at(Number);
+            Array filter(Function<filter_f>);
+            value_t reduce(Function<reduce_f>);
+            value_t at(Number);
 
 
             static constexpr class String __type__ = "object";
