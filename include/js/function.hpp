903888524301303214c3458c761cf492fba5dc51
2pichar <77556662+2pichar@users.noreply.github.com>
2022-01-06 11:55:05 -0800

    added basic Function class

--- include/js/function.hpp
+++ include/js/function.hpp
@@ -1,11 +1,21 @@
 #include <functional>
 namespace js {
-    class Function : public Object {
+    template <typename R, typename ...Args>
+    class Function<R(Args...)> : public Object {
+        typedef R return_t;
+        typedef R func_t(Args...);
         private:
-            std::function<> value;
+            std::function<func_t> value;
         public:
-            Function();
-            auto operator()();
-            
+            Function(func_t f){
+                this->value = f;
+            };
+            return_t operator()(Args... args){
+                return this->value(args...);
+            };
+            /*
+            return_t call(Object thisArg, Args... args){
+                return this->value(args...);
+            }*/
     }
 }
